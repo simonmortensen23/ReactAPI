8677f6d2fdf62fa74f99cc7357ac0f46867a8bef
simonmortensen23 <simon020393@gmail.com>
2022-09-15 10:49:57 +0000

    http requests

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import HTTPRequests from './components/HTTPRequests';
+
+function App() {
+  return (
+    <div className="App">
+      <HTTPRequests />
+    </div>
+  );
+}
+
+export default App;
