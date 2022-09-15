a3300457ca036a4dc3238025fa4a9aa5c0c16c4e
simonmortensen23 <simon020393@gmail.com>
2022-09-15 11:35:20 +0000

    HTTPHOOKS and API

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import HTTPRequests from './components/HTTPRequests';
+import HTTPPost from './components/HTTPPost';
+import HTTPHooks from './components/HTTPHooks';
 
 function App() {
   return (
     <div className="App">
-      <HTTPRequests />
+      <HTTPHooks />
+      {/* <HTTPRequests /> */}
+      {/* <HTTPPost /> */}
     </div>
   );
 }
